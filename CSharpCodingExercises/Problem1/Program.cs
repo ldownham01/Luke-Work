adc866bb1d8befc3d86edf61d375ee4a1a55a843
Luke Downham <ldownham2016@gmail.com>
2022-04-01 23:35:30 +0100

    commit 01/04/2022 23:35

---
+++ CSharpCodingExercises/Problem1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "1,2,1,1,0,3,1,0,0,2,4,1,0,0,0,0,2,1,0,3,1,0,0,0,6,1,3,0,0,0";
+            string[] numbers = input.Split(',');
+            int temp = 0;
+            int count = 0;
+
+            for (int i = 0; i<numbers.Length; i++)
+            {
+                if(numbers[i] == "0")
+                {
+                    temp++;
+                }
+                else
+                {
+                    temp = 0;
+                }
+
+                if (temp > count)
+                {
+                    count = temp;
+                }
+            }
+            Console.WriteLine("The longest number of days without sales is: {0}", count);
+            Console.ReadLine();
+        }
+    }
+}
