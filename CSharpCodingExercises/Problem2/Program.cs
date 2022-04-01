6e250fc6bcc75314f3e7b8a23ed9ff8ab32c50d7
Luke Downham <ldownham2016@gmail.com>
2022-04-02 00:24:48 +0100

    commit 02/04/2022
    Update to Problem2

--- CSharpCodingExercises/Problem2/Program.cs
+++ CSharpCodingExercises/Problem2/Program.cs
@@ -10,6 +10,23 @@ namespace Problem2
     {
         static void Main(string[] args)
         {
+            string input = "parts,traps,arts,rats,starts,tarts,rat,art,tar,tars,stars,stray";
+            string[] anagrams = input.Split(',');
+
+            for(int i = 0; i<anagrams.Length; i++)
+            {
+                if(anagrams[i].Length == 4 && charCount('s', anagrams[i]) == 1 && charCount('t', anagrams[i]) == 1 && charCount('a', anagrams[i]) == 1 && charCount('r', anagrams[i]) == 1)
+                {
+                    Console.WriteLine(anagrams[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static int charCount(char ch, string str)
+        {
+            int count = str.Split(ch).Length - 1;
+            return count;
         }
     }
 }
