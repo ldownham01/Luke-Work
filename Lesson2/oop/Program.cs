2c48b32247da15990001c0de7aff4f1392820721
Luke Downham <ldownham2016@gmail.com>
2022-04-01 17:50:31 +0100

    commit 01/04/2022 17:50
    Lesson 1 & Lesson 2 Update

---
+++ Lesson2/oop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class and create a new object
+            Calculator calc = new Calculator();
+            // Create a local variable to hold the answer
+            int x = 3;
+            int y = 5;
+            int sum = 0;
+            sum = calc.Add(x, y);
+            Console.WriteLine("sum of {1} + {2} = {3}", x, y, sum);
+            Console.ReadLine();
+        }
+    }
+}
