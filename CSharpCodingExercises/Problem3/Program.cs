cbc7087ed174ef50df1c73b0f08f63e40ecdb5d2
ldownham01 <ldownham2016@gmail.com>
2022-04-05 23:30:16 +0100

    commit 05/04/22 23:30
    Finished Problem3

---
+++ CSharpCodingExercises/Problem3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottom = 9;
+            // (bottom - i)/2 = number of spaces
+            // i = number of stars
+            for(int i = 1; i <= bottom; i = i + 2)
+            {
+                int spaces = ((bottom - i) / 2);
+
+                Console.WriteLine(" ");
+                  
+                for (int k = 0; k < spaces; k++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for (int l = 0; l < i; l++)
+                {
+                Console.Write("*");}
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
