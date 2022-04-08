2d339b4daef1fb31a086ea99c2c2af07b6b073ec
ldownham01 <ldownham2016@gmail.com>
2022-04-08 21:03:00 +0100

    commit 08/04/22 21:02
    Completed Coding Exercises - Problem 4

---
+++ CSharpCodingExercises/Problem4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottom = 9;
+            int spaces = 5;
+            // (bottom - i)/2 = number of spaces
+            // i = number of stars
+            for (int i = 1; i <= bottom; i = i + 2)
+            {
+                spaces = ((bottom - i) / 2);
+
+                Console.WriteLine(" ");
+
+                for (int k = 0; k < spaces; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int l = 0; l < i; l++)
+                {
+                    Console.Write("*");
+                }
+            }
+
+            for (int i = 7; i >= 0; i = i - 2)
+            {
+                spaces = ((bottom - i) / 2);
+
+                Console.WriteLine(" ");
+
+                for (int k = 0; k < spaces; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int l = 0; l < i; l++)
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
