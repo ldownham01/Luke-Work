8d0da1f123bc49db96ebdce11a50310e501a9b3c
ldownham01 <ldownham2016@gmail.com>
2022-04-10 21:32:22 +0100

    commit 10/04/2022
    completed Problem 6

---
+++ CSharpCodingExercises/Problem6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = isPalindrome("tenet"); ;
+            Console.WriteLine(input);
+            Console.ReadLine();
+        }
+
+        public static string reverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            string output = new string(arr);
+            return output;
+        }
+
+        public static string isPalindrome(string s)
+        {
+            if(s == reverseString(s))
+            {
+                return "Palindrome";
+            }
+            else
+            {
+                return "Not PalinDrome";
+            }
+        }
+    }
+}
