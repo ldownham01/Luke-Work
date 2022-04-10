f53fdae85dbc7619e7f8f1b0404f0ab28cb5857a
ldownham01 <ldownham2016@gmail.com>
2022-04-10 22:03:01 +0100

    commit 10/04/2022 22:02
    completed Problem 7

---
+++ CSharpCodingExercises/Problem7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 156;
+            int sum = 0;
+            int m;
+            while (n > 0)
+            {
+                m = n % 10;
+                sum = sum + m;
+                n = n / 10;
+            }
+            Console.Write(sum);
+            Console.ReadLine();
+        }
+    }
+}
--- CSharpCodingExercises/Problem7/Properties/AssemblyInfo.cs
+++ CSharpCodingExercises/Problem7/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Problem7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Problem7")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c69e87fa-e677-4f13-b4e7-8c890a5bcad7")]
+[assembly: Guid("00117fef-702e-40fd-b5df-2596e98c80d5")]
 
 // Version information for an assembly consists of the following four values:
 //
