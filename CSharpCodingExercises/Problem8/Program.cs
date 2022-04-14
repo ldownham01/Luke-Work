bda03efbc8dc03268686dab591ecc9a47afaee74
ldownham01 <ldownham2016@gmail.com>
2022-04-14 09:53:46 +0100

    commit 14/04/2022 09:53
    Completion of Problem8

---
+++ CSharpCodingExercises/Problem8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static Tuple<int, int> FindTwoSum(List<int> list, int sum)
+        {
+            for(int i = 0; i < list.Count; i++)
+            {
+                for(int j = 0; j < list.Count; j++)
+                {
+                    int temp = list[i] + list[j];
+                    if(temp == sum && i != j)
+                    {
+                        return Tuple.Create(list[i], list[j]);
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
