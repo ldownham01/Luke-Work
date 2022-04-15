a4cc8ea892128fdbc267945767735348b7ba7515
ldownham01 <ldownham2016@gmail.com>
2022-04-15 18:44:22 +0100

    commit 15/04/2022 18:44
    Lesson5 completed

---
+++ Lesson5/Overloading/Calculate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    internal class Calculate : Arithmetic
+    {
+        public float Add(float a, float b)
+        {
+            Console.WriteLine("Add floats called");
+            return a + b;
+        }
+
+        public String Add(String a, String b)
+        {
+            Console.WriteLine("Add strings called");
+            return "Hello " + a + " " + b;
+        }
+    }
+}
--- Lesson5/Overloading/Program.cs
+++ Lesson5/Overloading/Program.cs
@@ -10,6 +10,26 @@ namespace Overloading
     {
         static void Main(string[] args)
         {
+            Calculate calc = new Calculate();
+            
+            int a = 1;
+            int b = 2;
+            
+            float c = 3.0f;
+            float d = 4.0f;
+
+            double e = 5.0d;
+            double f = 6.0d;
+
+            String firstName = "Joe";
+            String LastName = "Bloggs";
+
+            Console.WriteLine("Add int: {0}", calc.Add(a, b));
+            Console.WriteLine("Add float: {0}", calc.Add(c, d));
+            Console.WriteLine("Add double: {0}", calc.Add(e, f));
+            Console.WriteLine("Add string : {0}", calc.Add(firstName, LastName));
+
+            Console.ReadLine();
         }
     }
 }
