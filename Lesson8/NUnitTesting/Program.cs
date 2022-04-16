693872be2fdd3cc4b040af85b098e539fc954104
ldownham01 <ldownham2016@gmail.com>
2022-04-16 19:37:03 +0100

    commit 16/04/2022 19:36

---
+++ Lesson8/NUnitTesting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lesson8/NUnitTesting/UnitTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    [TestFixture]
+    class UnitTest
+    {
+        [Test]
+        public void AddTest()
+        {
+            //Arrange
+            Arithmetic c = new Arithmetic();
+            int x = 4;
+            int y = 4;
+            double expectedValue = 8.0;
+            //Act
+            double actualValue = c.Add(x, y);
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        [Test]
+        public void SubtractTest()
+        {
+            //Arrange
+            Arithmetic c = new Arithmetic();
+            int x = 5;
+            int y = 4;
+            double expectedValue = 1.0;
+            //Act
+            double actualValue = c.Subtract(x, y);
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
