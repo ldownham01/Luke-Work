b2555dc8184d2f7bae3e7448eb1e8fcc02003360
ldownham01 <ldownham2016@gmail.com>
2022-04-16 11:07:36 +0100

    commit 16/04/2022 11:07
    Completed Lesson1 - Data Structures and Array Lists

---
+++ Lesson1 - Data Structures and ArrayLists/ArrayListExample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+            double average;
+            int grade = 0;
+            int total = 0;
+
+            //Instantiate an Array List
+            ArrayList grades = new ArrayList();
+            //Instantiate a random class
+            Random r = new Random();
+
+            for(int x = 0; x < 10; x++)
+            {
+                //generate a grade and add to arraylist
+                grade = (int) r.Next(0,100);
+                grades.Add(grade);
+            }
+
+            for(int i = 0; i < grades.Count; ++i)
+            {
+                //print out grades
+                Console.WriteLine("grades[" + i + "] = " + (int) grades[i]);
+                total += (int) grades[i];
+            }
+
+            average = total / grades.Count;
+            Console.WriteLine("The averages is " + average);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson1 - Data Structures and ArrayLists/ArrayListMaxNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListMaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare variables
+            double average;
+            int highest = 0;
+            int number = 0;
+
+            //Instantiate an Array List
+            ArrayList grades = new ArrayList();
+            //Instantiate a random class
+            Random r = new Random();
+
+            for (int x = 0; x < 10; x++)
+            {
+                //generate a grade and add to arraylist
+                number = (int)r.Next(0, 100);
+                grades.Add(number);
+            }
+
+            for (int i = 0; i < grades.Count; ++i)
+            {
+                //print highest grades
+                Console.WriteLine("grades[" + i + "] = " + (int)grades[i]);
+                if((int) grades[i] > highest)
+                {
+                    highest = (int) grades[i];
+                }
+            }
+
+            Console.WriteLine("The highest grade is " + highest);
+            Console.ReadLine();
+        }
+    }
+}
